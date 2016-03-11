b2b6ccb439aeb835190467092f16c4f02ec337bc
Liza Dashevski <liza.dashevski@gmail.com>
2016-03-11 11:50:47 +0200

    add context floating menu to items in listview

--- app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
+++ app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -2,7 +2,9 @@ package il.ac.huji.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -57,6 +59,8 @@ public class TodoListManagerActivity extends AppCompatActivity {
     }
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,9 +72,10 @@ public class TodoListManagerActivity extends AppCompatActivity {
         itemsAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
+        registerForContextMenu(lvItems);
 
 
-        setupListViewListener();
+     //   setupListViewListener();
 
 
     }
@@ -100,7 +105,23 @@ public class TodoListManagerActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.todo_context_floating_menu , menu);
+
+    }
+
     public void onAddItem(MenuItem v) {
+        EditText etNewItem = (EditText) findViewById(R.id.inputEditText);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    public void onDeleteItem(MenuItem v) {
         EditText etNewItem = (EditText) findViewById(R.id.inputEditText);
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
